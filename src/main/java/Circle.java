3fdd4bcfe2633f97f074b87358a95ab303df26f7
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-11 16:15:34 +0200

    Commit

---
+++ src/main/java/Circle.java
@@ -0,0 +1,21 @@
+public class Circle extends Shape {
+    private double radius;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*radius * Math.PI;
+    }
+}
